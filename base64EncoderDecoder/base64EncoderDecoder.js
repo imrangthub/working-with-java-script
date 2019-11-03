4ff8d96a9d3c5ae764090c67e9dcca8a90e8cead
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2019-11-03 22:57:34 +0600

    init project and add base64 encoding library

---
+++ base64EncoderDecoder/base64EncoderDecoder.js
@@ -0,0 +1,17 @@
+
+
+var textEncode = document.getElementById("txtEncode");
+var resultEndoce = document.getElementById("resultEecoded");
+
+var textDecode = document.getElementById("txtDncode");
+var resultDndoce = document.getElementById("resultDecoded");
+
+
+function doEncode(){
+    console.log("doEncode");
+}
+
+function doDncode(){
+    console.log("doDncode");
+}
+
