75de29e8fc332a7760273b22daa1a94f0dfd4608
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2019-11-05 06:29:31 +0600

    Update design

--- base64EncoderDecoder/base64EncoderDecoder.js
+++ base64EncoderDecoder/base64EncoderDecoder.js
@@ -1,17 +1,31 @@
 
 
-var textEncode = document.getElementById("txtEncode");
-var resultEndoce = document.getElementById("resultEecoded");
+function getBase64EncodedStr(rowStr){
+    let wordArray = CryptoJS.enc.Utf8.parse(rowStr);
+    let resultBase64Str = CryptoJS.enc.Base64.stringify(wordArray);
+    return resultBase64Str; 
+}
 
-var textDecode = document.getElementById("txtDncode");
-var resultDndoce = document.getElementById("resultDecoded");
+function getBase64DncodedStr(encStr){
+    let wordArray = CryptoJS.enc.Base64.parse(encStr);
+    let resultOriginalStr = wordArray.toString(CryptoJS.enc.Utf8);
+    return resultOriginalStr; 
+}
+
 
 
 function doEncode(){
     console.log("doEncode");
+    let textEncode = document.getElementById("txtEncode");
+    let resultEndoce = document.getElementById("resultEecoded");
+    resultEndoce.innerText = getBase64EncodedStr(textEncode.value);
 }
 
 function doDncode(){
     console.log("doDncode");
+    let textDecode = document.getElementById("txtDecode");
+    let resultDedoce = document.getElementById("resultDecoded");
+    resultDedoce.innerText = getBase64DncodedStr(textDecode.value);
+
 }
 
